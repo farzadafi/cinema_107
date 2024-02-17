c1a3e0ca041a2776fd43879040b3cd5acda5151f
mj-dehshiri <155631004+mj-dehshiri@users.noreply.github.com>
2024-02-18 02:16:54 +0330

    Add files via upload
    fix error

---
+++ Admin.java
@@ -0,0 +1,19 @@
+package org.cinema;
+
+import org.cinema.Person;
+
+public class Admin extends Person {
+
+    public Admin(String firstName,String lastName,String username,String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Admin(){
+
+    }
+
+}
